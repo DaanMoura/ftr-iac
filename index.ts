1ed769932b5653383fe570e5dea00c662e7c9cfe
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-02-26 09:13:25 -0300

    add ecr

--- index.ts
+++ index.ts
@@ -9,6 +9,17 @@ const bucket = new aws.s3.BucketV2(BUCKET_NAME, {
   },
 });
 
+const ecr = new aws.ecr.Repository("primeiro-ecr", {
+  name: "primeiro-ecr",
+  imageTagMutability: "IMMUTABLE",
+  tags: {
+    IAC: "true",
+  },
+});
+
 export const bucketName = bucket.id;
 export const bucketArn = bucket.arn;
 export const bucketRegion = bucket.region;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
